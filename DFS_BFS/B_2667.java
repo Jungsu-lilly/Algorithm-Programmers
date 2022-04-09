68bf5045ca3dc3fc40879133427126e436a74819
matt1235 <matt1235@naver.com>
2022-04-09 14:16:21 +0900

    단지 번호 붙이기

---
+++ DFS_BFS/B_2667.java
@@ -0,0 +1,50 @@
+package DFS_BFS;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class B_2667 {
+    static int N = 0;
+    static int[][]a = new int[25][25];
+    static ArrayList<Integer> list = new ArrayList<>();
+    static boolean visited[][] = new boolean[25][25];
+    static int max = 0;
+    public void dfs(int x, int y){
+        max++;
+        visited[x][y] = true;
+        if(x-1>=0 && !visited[x-1][y] && a[x-1][y] == 1) {dfs(x-1,y);}
+        if(y+1<N && !visited[x][y+1] && a[x][y+1] == 1) {dfs(x,y+1);}
+        if(x+1<N && !visited[x+1][y] && a[x+1][y] == 1) {dfs(x+1,y);}
+        if(y-1>=0 && !visited[x][y-1] && a[x][y-1] == 1) {dfs(x,y-1);}
+    }
+
+    public static void main(String[] args) {
+        B_2667 b = new B_2667();
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        for(int i=0;i<N;i++) {
+            String s = "";
+            s = sc.next();
+            for(int j=0;j<N;j++){
+                a[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(!visited[i][j] && a[i][j]==1) {
+                    b.dfs(i,j);
+                    list.add(max);
+                    max = 0;
+                }
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.size());
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+}
