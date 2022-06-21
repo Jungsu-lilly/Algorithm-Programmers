623284ecb6165bfc5bb3c6c45fac632ee2ad918a
matt <matt1235@naver.com>
2022-06-21 18:37:14 +0900

    완전탐색 알고리즘 - 순열(Permutation), 재귀

--- 완전탐색/P_42839.java
+++ 완전탐색/P_42839.java
@@ -3,35 +3,35 @@ import java.util.*;
 
 // 프로그래머스 - [소수 찾기]
 public class P_42839 {
-    public int solution(String numbers) {
-        int ans = 0;
-        HashSet<Integer> set = new HashSet<>();
-        permutation("",numbers,set);
+    static Set<Integer> set = new HashSet<>();
 
-        for (Integer i : set) {
-            if(i==2) ans++;
-            if(i%2 != 0 && isPrime(i)) ans++;
-        }
-        return ans;
-    }
-
-    // 소수 판별
     public boolean isPrime(int n){
         if(n==0 || n==1) return false;
 
+        // 홀수만 체크!
         for(int i=3;i<=(int)Math.sqrt(n);i+=2){
             if(n%i==0) return false;
         }
         return true;
     }
 
-    public void permutation(String prefix, String num, HashSet<Integer> set){
-        if(!prefix.equals(""))
-            set.add(Integer.valueOf(prefix));
-        int n = num.length();
-        for(int i=0;i<n;i++){
-            permutation(prefix+num.charAt(i),num.substring(0,i)+num.substring(i+1,n), set);
+    public void permutation(String temp, String s){
+        if(!temp.isBlank()) set.add(Integer.parseInt(temp));
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            permutation(temp+c,s.substring(0,i)+s.substring(i+1,s.length()));
         }
+    }
+
+    public int solution(String numbers) {
+        int answer = 0;
+        permutation("", numbers);
+        for (Integer i : set) {
+            if(i==2) answer++;
+            if(i%2!=0 && isPrime(i)) answer++;
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
