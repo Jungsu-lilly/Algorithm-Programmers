fb53e0d53d673514e73784751470e8dd0a5f89c4
matt <matt1235@naver.com>
2022-06-27 02:34:59 +0900

    no message

--- Solution.java
+++ Solution.java
@@ -1,61 +1,36 @@
 import java.util.*;
 
 class Solution{
-    public int[] solution(String[] genres, int[] plays) {
-        int k =0;
-        Map<String, ArrayList<int[]>> genreMap = new HashMap<>();
-        Map<String, Integer>total = new HashMap<>();
+    public String solution(int[] numbers) {
+        String answer = "";
+        int numOfZero = 0;
 
-        int length = genres.length;
-        List<Integer> list = new ArrayList<>();
-
-        for(int i=0;i<length;i++){
-            ArrayList<int[]> temp = genreMap.getOrDefault(genres[i], new ArrayList<>());
-            temp.add(new int[]{i, plays[i]});
-            genreMap.put(genres[i],temp);
-            total.put(genres[i],total.getOrDefault(genres[i], 0)+plays[i]);
+        List<String> list = new ArrayList<>();
+        for (int n : numbers) {
+            if(n!=0) list.add(Integer.toString(n));
+            numOfZero++;
         }
 
-        ArrayList<Map.Entry<String, Integer>> totalPlays = new ArrayList<>(total.entrySet());
-        Collections.sort(totalPlays, new Comparator<Map.Entry<String, Integer>>() {
+        if(numOfZero == numbers.length) return "0";
+
+        Collections.sort(list, new Comparator<String>() {
             @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o2.getValue() - o1.getValue();
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
             }
         });
 
-        for (Map.Entry<String, Integer> s : totalPlays) {
-            String key = s.getKey();
-            ArrayList<int[]> playList = genreMap.get(key);
+        for (String s : list)
+            answer+=s;
 
-            Collections.sort(playList, new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return o2[1] - o1[1];
-                }
-            });
+        for(int i=0;i<numOfZero;i++)
+            answer+="0";
 
-            for(int i=0;i<playList.size();i++){
-                if(i==2) break;
-                list.add(playList.get(i)[0]);
-            }
-        }
-
-        int[]ans = new int[list.size()];
-
-        // list -> array
-        for(int i=0;i<list.size();i++){
-            ans[i]=(int)list.get(i);
-        }
-        return ans;
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] ans = s.solution(new String[]{"classic", "pop", "classic", "classic", "pop"},
-                new int[]{500, 600, 150, 800, 2500});
-        for (int a : ans) {
-            System.out.print(a+", ");
-        }
+
     }
 }
